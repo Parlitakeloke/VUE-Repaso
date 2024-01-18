60fff5e17e9a095b7884a7e3453f6a9c28f541d9
Parlitakeloke <trollert33@gmail.com>
2024-01-18 12:54:14 +0100

    Me cago

---
+++ vue-project/src/components/Ejercicio1.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>HOLA MUNDO CON VUE</h1>
+    <input type="text" name="fruta" id="campoFruta" v-model="frutika">
+    <button @click="agregarFruta">Agregar</button>
+    <ul>
+        <li v-for="(fruta, index) in frutas">
+            {{ index }} - {{ fruta.nombre }}
+            <small>({{ fruta.cantidad }}) <p v-if="fruta.cantidad <= 0">Sin Stock</p></small>
+        </li>
+    </ul>
+
+    <h1>TOTAL: </h1>
+</template>
+
+<script>
+export default {
+    name: 'Ejercicio1',
+    components: {},
+    data() {
+        return {
+
+            frutas: [
+                { nombre: 'Pera', cantidad: '10' },
+                { nombre: 'Manzana', cantidad: '20' },
+                { nombre: 'Patata', cantidad: '0' },
+            ],
+            frutika : ""
+            
+        }
+    },
+    methods: {
+
+        agregarFruta(){
+
+            let objetiko =  {nombre: this.frutika, cantidad: 0};
+            this.frutas.push(objetiko);
+
+        }
+
+    }
+}
+</script>
---
+++ vue-project/src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import Ejercicio1 from './Ejercicio1.vue'
+
+createApp(App).mount('#app')
+// createApp(Ejercicio1).mount('#Ejercicio1')
